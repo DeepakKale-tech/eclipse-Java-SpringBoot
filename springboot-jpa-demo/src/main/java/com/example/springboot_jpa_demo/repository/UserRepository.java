a8f19c289bf52e29c77ee4dd1b2c2ca65396c8b6
Deepak Kale <kaledeepak4646@gmail.com>
2026-03-14 17:48:57 +0530

    Spring Boot JPA commit

---
+++ springboot-jpa-demo/src/main/java/com/example/springboot_jpa_demo/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.springboot_jpa_demo.repository;
+
+import java.util.function.Function;
+
+import org.springframework.data.domain.Example;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.FluentQuery.FetchableFluentQuery;
+
+import com.example.springboot_jpa_demo.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+	
+}
